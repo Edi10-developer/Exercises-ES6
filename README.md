638f3a5714ff3acb8584a1427a236cf52071e2fb
Edi <selimi.edi.1991@gmail.com>
2021-03-04 15:25:57 +0100

    completed level 3

---
+++ README.md
@@ -0,0 +1,51 @@
+
+# ES6 Ezercises
+
+> Three levels of [IT Academy](https://itacademy.barcelonactiva.cat/) Exercise  using EcmaScript and the functions of Modern JavaScript.
+
+## Live Demo
+
+[Live Site](https://es6-exercises.netlify.app/)
+
+
+### View:
+
+ **Level 1**
+![Image](./img/console.png)
+
+ **Level 2**
+![Image](./img/model1.png)
+
+ **Level 3**
+![Image](./img/model2.png)
+
+## Built With
+
+- HTML
+- CSS ([Font Awesome](https://fontawesome.com/v4.7.0/))
+- JavaScript
+
+## Getting Started
+
+Open index.html with your Browser.
+
+
+## Author
+
+👤 **Author**
+- GitHub: [@Edi10-developer](https://github.com/Edi10-developer)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/edi-selimi-856671173/?locale=en_US)
+- Portfolio: [www.ediselimi.com](https://ediselimi.com/)
+
+
+## Show your support
+
+Contributions, issues, and feature requests are welcome!
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
--- js/main.js
+++ js/main.js
@@ -174,7 +174,36 @@ const calculation2 = () => {
     tableBody.appendChild(row);
     table.appendChild(tableBody);
     document.querySelector('.result').appendChild(table);
-    // tbl border attribute to 
     table.classList.add("table");
 }
 
+
+
+/***********************
+     L E V E L   3
+***********************/
+console.log('** + ** + ** + ** + ** + LEVEL 3 + ** + ** + ** + ** + **');
+console.log('      ');
+console.log('      ');
+console.log('****************  Exercise 9  *******************');
+
+var names = ['Albert', 'Ismael', 'Luis', 'Ruben', 'Jonatan', 'Jake', 'Jose'];
+
+const filterNames = () => {
+  var input, filter, ul, li, a, i, txtValue;
+  
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
